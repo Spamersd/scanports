5d7a27f078c3c988a0e878252eda9fb35258c5bb
Spamersd <spamersd@gmail.com>
2021-06-02 09:40:31 +0700

    master

--- scanport_0.1.ps1
+++ scanport_0.1.ps1
@@ -6,7 +6,18 @@ class Hosts {
 $ips=@{}
 $dt = @()
 foreach($line in Get-Content ".\IN.txt") {
+    
+    if ($line -notmatch "([0-9]{1,3}[\.]){3}[0-9]{1,3}") {
+        
+        continue
+        
+    }
     foreach($port in Get-Content ".\ports.txt"){
+        
+        if ($port -notmatch "^\d+$") {
+            continue
+        }
+
         $myhost = New-Object -TypeName Hosts
         $myhost.hostIP = $line
         $myhost.port = $port
