7e2b74deee26009900c99d636e6995b7114d8957
Spamersd <spamersd@gmail.com>
2021-06-15 17:15:51 +0700

    add clock and error log

--- scanport_0.1.ps1
+++ scanport_0.1.ps1
@@ -1,8 +1,10 @@
-
+$watch = [System.Diagnostics.Stopwatch]::StartNew()
+$watch.Start() 
 # Params 
 $showempty = $true
 $PathHostList = ".\IN.txt"
 $PathPortList = ".\ports.txt"
+$errorLOG =@()
 
 class Hosts {
     $hostIP
@@ -11,27 +13,33 @@ class Hosts {
 
 $ips=@{}
 $dt = @()
+$ListPort = @();
+
+foreach($port in Get-Content $PathPortList){
+    if ($port -notmatch "^\d+$") {
+        $errorLOG += "'$port' is not port"
+        continue
+        
+    }
+    $ListPort += $port
+}
 
 foreach($line in Get-Content $PathHostList) {
     
     if ($line -notmatch "([0-9]{1,3}[\.]){3}[0-9]{1,3}") {
-        continue    
-    }
-    foreach($port in Get-Content $PathPortList){
+        $errorLOG += "'$line' is not IP adress" 
+        continue   
         
-        if ($port -notmatch "^\d+$") {
-            continue
-        }
-
+    }
+    foreach($port in  $ListPort){
         $myhost = New-Object -TypeName Hosts
         $myhost.hostIP = $line
         $myhost.port = $port
         $dt += $myhost
     }
 } 
-$dt
-$dt | ForEach-Object -Parallel {
-  $data += tnc $_.hostIP -p $_.port -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+$data = $dt | ForEach-Object -Parallel {
+   tnc $_.hostIP -p $_.port -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
 } 
 foreach ($item in $data) {
     if (($ips[$item.ComputerName] -eq $null) -and $showempty) {
@@ -44,5 +52,9 @@ foreach ($item in $data) {
         $ips[$item.ComputerName] += $item.RemotePort    
     } 
 }
+$errorLOG
 
 $ips.GetEnumerator() | sort {[version] $_.Name}
+$watch.Stop()
+
+"Run time: "+ $watch.Elapsed
