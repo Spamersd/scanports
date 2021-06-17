b2df4d3e276bc76d9bc22e3ef114f0520c31d26f
Spamersd <spamersd@gmail.com>
2021-06-17 11:56:15 +0700

    В разы повышена производительность

--- scanport_0.1.ps1
+++ scanport_0.1.ps1
@@ -1,7 +1,7 @@
 $watch = [System.Diagnostics.Stopwatch]::StartNew()
 $watch.Start() 
 # Params 
-$showempty = $true
+$showempty = $false
 $PathHostList = ".\IN.txt"
 $PathPortList = ".\ports.txt"
 $errorLOG =@()
@@ -15,6 +15,27 @@ $ips=@{}
 $dt = @()
 $ListPort = @();
 
+function NetPortTest {
+    param (
+        [String]$IPaddr,
+        [Int]$Port,
+        [Int]$Timeout
+    )
+    begin {
+        $result = @()
+    }
+    process{
+        $tcpClient = New-Object System.Net.Sockets.TcpClient
+        $result = $tcpClient.ConnectAsync($remoteHostname, $remotePort).Wait($Timeout)    
+    }
+    end{
+        return $result
+    }
+    
+}
+
+
+
 foreach($port in Get-Content $PathPortList){
     if ($port -notmatch "^\d+$") {
         $errorLOG += "'$port' is not port"
@@ -26,35 +47,71 @@ foreach($port in Get-Content $PathPortList){
 
 foreach($line in Get-Content $PathHostList) {
     
-    if ($line -notmatch "([0-9]{1,3}[\.]){3}[0-9]{1,3}") {
+    if ($line -match "([0-9]{1,3}[\.]){3}[0-9]{1,3}(?=\s|$)") {
+        
+        foreach($port in  $ListPort){
+            $myhost = New-Object -TypeName Hosts
+            $myhost.hostIP = $line
+            $myhost.port = $port
+            $dt += $myhost
+        }
+    }
+    elseif ($line -match "([0-9]{1,3}[\.]){3}[0-9]{1,3}[\/][0-9]{2}(?=\s|$)") {
+        $net = $line -split "/"
+        $net
+    }
+    else {
         $errorLOG += "'$line' is not IP adress" 
-        continue   
+        continue           
+    }
+    
+} 
+
+
+$data = $dt | Foreach-Object -Parallel {
+    function NetPortTest {
+        param (
+            [String]$IPaddr,
+            [Int]$Port,
+            [Int]$Timeout
+        )
+        begin {
+            $result = @{
+                ComputerName       = $IPaddr
+                RemotePort           = $Port
+                TcpTestSucceeded     = $false
+               }
+        }
+        process{
+            $tcpClient = New-Object System.Net.Sockets.TcpClient
+            $result["TcpTestSucceeded"] = $tcpClient.ConnectAsync($IPaddr, $Port).Wait($Timeout)
+            }  
+        
+        end{
+            return $result
+        }
         
     }
-    foreach($port in  $ListPort){
-        $myhost = New-Object -TypeName Hosts
-        $myhost.hostIP = $line
-        $myhost.port = $port
-        $dt += $myhost
-    }
+    #tnc $_.hostIP -p $_.port -ErrorAction SilentlyContinue -WarningAction SilentlyContinue # Wery wery slowly
+    NetPortTest -IPaddr $_.hostIP -Port $_.port -Timeout 1000 #wery fast
 } 
-$data = $dt | ForEach-Object -Parallel {
-   tnc $_.hostIP -p $_.port -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
-} 
+
 foreach ($item in $data) {
-    if (($ips[$item.ComputerName] -eq $null) -and $showempty) {
+    if (($null -eq $ips[$item.ComputerName]) -and $showempty) {
             $ips[$item.ComputerName]=@() 
         } 
     if ($item.TcpTestSucceeded -eq $true) { 
-        if ($ips[$item.ComputerName] -eq $null) {
+        if ($null -eq $ips[$item.ComputerName]) {
             $ips[$item.ComputerName]=@() 
         }
         $ips[$item.ComputerName] += $item.RemotePort    
     } 
 }
+
 $errorLOG
 
-$ips.GetEnumerator() | sort {[version] $_.Name}
-$watch.Stop()
+$ips.GetEnumerator() | Sort-Object {[version] $_.Name}
 
+$watch.Stop()
+"Call request: " + $dt.Count
 "Run time: "+ $watch.Elapsed
