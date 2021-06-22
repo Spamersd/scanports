8b48903d6c2660a8f40db4a510bdaf49fc01b558
Spamersd <spamersd@gmail.com>
2021-06-22 13:54:10 +0700

    add ip range

--- TestPort.ps1
+++ TestPort.ps1
@@ -1,2 +1,25 @@
-$hash = @{"10.18.50.55" = @(21, 22, 80, 23,55,5000); "10.18.50.6" = @(21, 22, 80, 23,55,5000)  }
-$hash | ForEach-Object {"{0}`t{1}" -f $_.Name,($_.Value -join ",")}
+
+$line = @("1.2.3.4", "22")
+$ip =[IPAddress]$line[0]
+$mask = [IPAddress](ConvertTo-IPv4MaskString $line[1])
+$net = [IPAddress]($ip.Address -band $mask.Address)
+$broad = [IPAddress]($ip.Address -bor (-bnot [uint]$mask.Address))
+
+
+
+# $net 
+# $mask 
+# $broad 
+# $net_b..($net_b+$range) | ForEach-Object{
+#      [ipaddress](ReversIP -IP $_)
+# }
+
+
+
+# $ip_int = ConvertIP_ToBit -IP "192.168.10.168"
+# $mask_int = ConvertIP_ToBit -IP $mask.IPAddressToString
+
+# $count = 
+
+
+  
--- scanport_0.1.ps1
+++ scanport_0.1.ps1
@@ -11,6 +11,36 @@ class Hosts {
     $port
 }
 
+function GetIPrange {
+    param (
+      [string]$start,
+      [string]$stop
+    )
+    $IP_start = $start -split "\."
+    $IP_stop = $stop -split "\."
+  
+    $ips = @()
+    foreach ($okt0 in ($IP_start[0]..$IP_stop[0])) {   
+    foreach ($okt1 in ($IP_start[1]..$IP_stop[1])) {
+    foreach ($okt2 in ($IP_start[2]..$IP_stop[2])) {
+    foreach ($okt3 in ($IP_start[3]..$IP_stop[3])) {
+      $ips += "$okt0.$okt1.$okt2.$okt3"
+    }}}}
+    return $ips
+    
+  }
+
+  function ConvertTo-IPv4MaskString {
+    param(
+      [Parameter(Mandatory = $true)]
+      [ValidateRange(0, 32)]
+      [Int] $MaskBits
+    )
+    $mask = ([Math]::Pow(2, $MaskBits) - 1) * [Math]::Pow(2, (32 - $MaskBits))
+    $bytes = [BitConverter]::GetBytes([UInt32] $mask)
+    (($bytes.Count - 1)..0 | ForEach-Object { [String] $bytes[$_] }) -join "."
+
+}
 $ips = @{}
 $dt = @()
 $ListPort = @();
@@ -26,7 +56,6 @@ foreach ($port in Get-Content $PathPortList) {
 }
 
 foreach ($line in Get-Content $PathHostList) {
-    
     if ($line -match "([0-9]{1,3}[\.]){3}[0-9]{1,3}(?=\s|$)") {
         
         foreach ($port in  $ListPort) {
@@ -40,8 +69,19 @@ foreach ($line in Get-Content $PathHostList) {
     elseif ($line -match "([0-9]{1,3}[\.]){3}[0-9]{1,3}[\/][0-9]{2}(?=\s|$)") {
     
         $net = $line -split "/"
-        $net
-    
+        $ip =[IPAddress]$net[0]
+        $mask = [IPAddress](ConvertTo-IPv4MaskString $net[1])
+        $net = [IPAddress]($ip.Address -band $mask.Address)
+        $broad = [IPAddress]($ip.Address -bor (-bnot [uint]$mask.Address))
+        $list = GetIPrange -start $net.IPAddressToString -stop $broad.IPAddressToString
+        foreach ($ip in $list) {
+            foreach ($port in  $ListPort) {
+                $myhost = New-Object -TypeName Hosts
+                $myhost.hostIP = $ip
+                $myhost.port = $port
+                $dt += $myhost
+            }
+        }
     }
     else {
     
@@ -91,7 +131,6 @@ foreach ($item in $data) {
     } 
 }
 
-Clear-Host
 
 $errorLOG
 $ips.GetEnumerator() | Sort-Object { [version] $_.Name } | ForEach-Object {"{0}`t{1}" -f $_.Name,($_.Value -join ", ")}
